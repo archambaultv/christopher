b94b408187995a2a230303100f5e9c6c7415f5e6
Vincent Archambault <vincentarchambault@icloud.com>
2022-10-07 12:27:28 -0400

    Basic simulations

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
+-- |
+-- Module      :  Main
+-- Copyright   :  © 2022 Vincent Archambault
+-- License     :  MIT
+--
+-- Maintainer  :  Vincent Archambault <vincentarchambault@icloud.com>
+-- Stability   :  experimental
+--
+-- The main module of the executable
+
 module Main (main) where
 
-import Lib
-
 main :: IO ()
-main = someFunc
+main = putStrLn msg
+
+msg :: String
+msg = "Hello my name is Christopher. I am a simple tool to help you plan your financial future.\n\n"
+   ++ "Looks like your are running the default command line executable. "
+   ++ "For now, in order to use this tool you need to code your own simulation (see the Simulation data type) and modify the main function.\n\n"
+   ++ "I suggest you do so in your own private branch, since your simulations might contain personnal financial data."
---
+++ src/Christopher/Simulation.hs
@@ -0,0 +1,99 @@
+-- |
+-- Module      :  Christopher.CLI
+-- Copyright   :  © 2022 Vincent Archambault
+-- License     :  MIT
+--
+-- Maintainer  :  Vincent Archambault <vincentarchambault@icloud.com>
+-- Stability   :  experimental
+--
+-- This module defines what a financial planning simulation is and how to run it
+
+module Christopher.Simulation
+(
+  Simulation,
+  SimulationInfo(..),
+  SimulationResults,
+  Assets,
+  Asset(..),
+  AccountType(..),
+  Rate,
+  YearlyDatum(..),
+  FiscalDatum(..),
+  TaxBrackets(..),
+  SState,
+  Strategy(..),
+  runSimulation
+)
+where
+
+import Data.Decimal
+import Data.Functor.Foldable
+import Control.Monad.State
+
+data SimulationInfo = SimulationInfo {
+  infoAssets :: Assets,
+  infoYearlyData :: [YearlyDatum]
+} deriving (Show, Eq)
+
+type SimulationResults = Assets
+
+type Assets = Asset
+
+data AccountType 
+  = RRSP -- REER
+  | TFSA -- CELI
+  deriving (Show, Eq)
+
+data Asset = Asset {
+  aType :: AccountType,
+  aBalance :: Decimal
+} deriving (Show, Eq)
+
+type Rate = Rational
+
+data YearlyDatum = YearlyDatum {
+  fiscalDatum :: FiscalDatum,
+  yield :: Rate
+} deriving (Show, Eq)
+
+data FiscalDatum = FiscalDatum {
+  income :: Decimal,
+  needForSpending :: Decimal,
+  taxBrackets :: TaxBrackets
+} deriving (Show, Eq)
+
+data TaxBrackets
+  = TaxFinalBracket Decimal Rate
+  | TaxBracket (Decimal, Decimal) Rate TaxBrackets
+ deriving (Show, Eq)
+
+type SState = State Assets
+
+data Strategy = Strategy {
+  sName :: String,
+  sPayTaxesAndInvest :: FiscalDatum -> SState ()
+} 
+
+instance Show Strategy where
+  show s = show (sName s)
+
+type Simulation = (Strategy, SimulationInfo)
+
+runSimulation :: Simulation -> SimulationResults
+runSimulation (strat, info) = 
+  let simul = cata alg (infoYearlyData info)
+  in evalState simul (infoAssets info)
+
+  where
+    alg :: ListF YearlyDatum (SState SimulationResults) -> (SState SimulationResults)
+    alg Nil = get >>= pure
+    alg (Cons datum next) = do
+      sPayTaxesAndInvest strat (fiscalDatum datum)
+      simulateMarket (yield datum)
+      next
+
+simulateMarket :: Rate -> SState ()
+simulateMarket r = do
+  a <- get
+  let newBalance = roundTo 2 (aBalance a *. (1 + r))
+  put a{aBalance = newBalance}
