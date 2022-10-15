95ba724de247805102d75137f1b9352b65d21b91
Vincent Archambault-Bouffard <vincentarchambault@icloud.com>
2022-10-15 00:25:24 -0400

    Implement TaxTable

--- src/Christopher/Report/TaxTable.hs
+++ src/Christopher/Report/TaxTable.hs
@@ -52,4 +52,30 @@ instance DefaultOrdered TaxTableRow where
   headerOrder = genericHeaderOrder csvOptions
 
 taxTable :: IncomeTaxInfo -> [TaxTableRow]
-taxTable = undefined
+taxTable info =
+  let incomes = [x * 1000 | x <- [10..80]] -- Up to 80K
+              ++ [80000 + x * 5000 | x <- [1..24]] -- Up to 200K
+              ++ [200000 + x * 10000 | x <- [1..20]] -- Up to 400K
+  in map (\x -> reportToTableRow (computeTax info $ TaxReportInput (salary x) 0,
+                                  computeTax info $ TaxReportInput (salary (x + 1)) 0)) 
+     incomes
+
+reportToTableRow :: (TaxReport, TaxReport) -> TaxTableRow
+reportToTableRow (tr, marginalTr) =
+  let taxableIncome = iSalary $ tiIncome $ trTaxReportInput tr
+      fedTax = trFedIncomeTax tr
+      qcTax = trQcIncomeTax tr
+      totalTax = fedTax + qcTax
+      effectiveRate = toRate $ roundTo 4 $ totalTax / taxableIncome
+
+      fedTax2 = trFedIncomeTax marginalTr
+      qcTax2 = trQcIncomeTax marginalTr
+      totalTax2 = fedTax2 + qcTax2
+      fedMarginalRate = toRate $ roundTo 4 $ fedTax2 - fedTax
+      qcMarginalRate = toRate $ roundTo 4 $ qcTax2 - qcTax
+      totalMarginalRate = toRate $ roundTo 4 $ totalTax2 - totalTax
+  in TaxTableRow taxableIncome 
+                 (roundTo 0 fedTax) 
+                 (roundTo 0 qcTax)
+                 (roundTo 0 totalTax) 
+                 effectiveRate fedMarginalRate qcMarginalRate totalMarginalRate
